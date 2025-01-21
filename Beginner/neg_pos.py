7125cfe85059923465cdc6218682f1a6ea7ba2cd
dhiflaouik <belgacem.dhiflaoui@gmail.com>
2025-01-21 22:17:31 +0100

    feat: add script to check if a number is positive, negative, or zero

---
+++ Beginner/neg_pos.py
@@ -0,0 +1,11 @@
+"""
+Project Description
+Create a simple program that checks if a given number is positive, negative, or zero.
+"""
+variable = int(input("Enter a number: "))
+if variable > 0:
+    print("the Number is Positive")
+elif variable < 0:
+    print("the Number is Negative")
+else:
+    print("the Number is a Zero")
